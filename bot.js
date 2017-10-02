431caf27a1fb866001444760f2b4671e2a2b5fd4
Silver0034 <airraid.mail@gmail.com>
2017-10-01 22:54:53 -0400

    Add files via upload

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ var taste = require('./commandFunctions/taste.js');
 var profanity = require('./commandFunctions/profanity.js');
 var cleanDictionary = require('./commandFunctions/cleanDictionary.js');
 var languageResponse = require('./commandFunctions/languageResponse.js');
-var $ = require('./jquery-3.2.1.min.js');
+var $ = require('https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js');
 
 //establish global variables and constants
 const TOKEN = tokenReturn.return();
