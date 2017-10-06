3d833aad1dfa86909d1eb60222d5fa11b6add8ea
Silver0034 <airraid.mail@gmail.com>
2017-10-06 01:45:03 -0400

    Add files via upload

--- bot.js
+++ bot.js
@@ -374,7 +374,7 @@ commandDictionary['admin'] = {
           	//remove profanity filter from channel
           	sqldb.query("UPDATE channel SET profanityMonitor = 0 WHERE channelID = " + message.channel.id, function (err, results, fields) {
   						if (err) throw err;
-              return responseHead(message, key) + 'The profanity filter has been removed from this channel.';
+              //return responseHead(message, key) + 'The profanity filter has been removed from this channel.';
         			console.log(results);
       			});
       			console.log('Removed profanity filter from channel ' + message.channel.name);
@@ -382,10 +382,10 @@ commandDictionary['admin'] = {
           	//add profanity filter from channel
           	sqldb.query("UPDATE channel SET profanityMonitor = 1 WHERE channelID = " + message.channel.id, function (err, results, fields) {
   						if (err) throw err;
-              return responseHead(message, key) + 'The profanity filter has been added to this channel.';
+              //return responseHead(message, key) + 'The profanity filter has been added to this channel.';
         			console.log(results);
       			});
-      			console.log('Added profanity filter to channel ' + message.channel.name);  
+      			console.log('Added profanity filter to channel ' + message.channel.name);
       		} else {
           	return error(key); // TODO: append more description later
       		}
