f078feb37b8f127b591dbebdacfcac8848a916b6
Silver0034 <airraid.mail@gmail.com>
2017-10-06 11:54:42 -0400

    Add files via upload

--- commandFunctions/profanity.js
+++ commandFunctions/profanity.js
@@ -196,7 +196,7 @@ var spaceCheck = function (messageCheck) {
   return combineSpaces;    
 }
 
-exports.filter = function(message, emojiDino) {
+exports.filter = function(message, emojiDino, getTime, getDate {
 	//check if theres spaces in the middle of curse words
 	var messageCheck = message.content.split(' ');
 	var messageSpaceCheck = spaceCheck(messageCheck);
@@ -216,11 +216,13 @@ exports.filter = function(message, emojiDino) {
 					if(messageCheck[i].indexOf(profanityExceptions[k]) != -1) {return;} 
 				}   
 				if (message.guild) {
-					message.channel.send(emojiDino + profanityResponseGenerate());       
+					message.channel.send(emojiDino + profanityResponseGenerate());      
+					console.log(getTime(date), message.author.username + ' was warned about cursing.');    
 					//message.author.send(emojiDino + '<@' + userID + '>, please keep the ' + message.guild.name + ' profanity free. Do not curse.');     
 					message.guild.owner.send(emojiDino + ' ' + message.author.username + ' cursed in your server, ' + message.guild.name + ', in the channel ' + message.channel.name +':```' + '\n' + message.author.username + ': \"' + message + '\"```' + 'The trigger was ' + profanityList[j] + '\non ' + getDate());  
 					return;
 				} else {
+					console.log(getTime(date), message.author.username + ' cursed at me in direct message');
 					message.author.send(emojiDino + '<@' + userID + '>, please don\'t curse in front of me. :confounded: ');
 					return;
 				}
