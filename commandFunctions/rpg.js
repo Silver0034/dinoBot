23fc985e81769a4887209d445b2b7bb11f295727
Silver0034 <airraid.mail@gmail.com>
2017-10-07 13:16:14 -0400

    Add files via upload

--- commandFunctions/rpg.js
+++ commandFunctions/rpg.js
@@ -200,11 +200,24 @@ rpgConditions['charmed'] = {
 rpgConditions['deafened'] = {
   desc: 'A deafened creature can\'t hear and automatically fails any check that requires hearing.'
 };
+rpgConditions['exhaustion'] = {
+	desc: 'Some special abilities and enviromental hazards, such as starvation and the long-term effects of freezing or scorching temperatures, can lead to a special condition called exhaustion. Exhaustion is measured in six levels. An effect can give a creature one or more levels of exhaustion, as specified in the effect\'s description.\n' +
+	'Level 1: Disadvantage on ability checks' +
+	'Level 2: Speed halved' +
+	'Level 3: Disadvantage on attack rolls and saving throws' +
+	'Level 4: Hit point maximum halved' +
+	'Level 5: Speed reduced to 0' +
+	'Level 6: Death' +
+	'If an already exhausted creature suffers another effect that causes exhaustion, its current level of exhaustion increases by the amount specified in the effect\'s descripition.\nA creature suffers the effect of its current level of exhaustion as well as all lower levels. For example, a creature suffering level 2 exhaustion has its speed halved and has disadvantage on ability checks.\nAn effect that removes exhaustion reduces its level as specified in the effect\'s description, with all exhaustion effects ending if a creature\'s exhaustion level is reduced below 1.\nFinishing a long rest reduces a creature\'s exhaustion level by 1, provided that the creature has also ingested some food and drink.'
+};
 rpgConditions['frightened'] = {
   desc: 'A frightened creature has disadvantage on ability checks and attack rolls whil the source of its fear is within line of sight.\nThe creature can\'t willingly move closer to the source of its fear.'
 };
 rpgConditions['grappled'] = {
   desc: 'A grappled creature\'s speed becomes 0, and it can\'t benefit from any bonus to its speed.\nThe condition ends if hte grappler is incapacitated (see the condition).\nThe condition also ends if an efefct removes the grappled creature from the reach of the grappler or grappling effect, such as when a creature is hurled away by the *thunderwave* spell.'
+};
+rpgConditions['incapacitated'] = {
+	desc: 'An incapacitated creature can\'t take actions or reactions.'
 };
 rpgConditions['invisible'] = {
   desc: 'An invisible creature is impossible to see without the aid of magic or a special sense. For the purpose of hiding, the creature is heavily obscured. The creature\'s location can be detected by any noise it makes or any tracks it leaves.\nAttack rolls against the creature have disadvantage, and the creature\'s attack rolls have advantage.'
@@ -218,10 +231,18 @@ rpgConditions['petrified'] = {
 rpgConditions['poisoned'] = {
   desc: 'A poisoned creature has disadvantage on attack rolls and ability checks.'
 };
+rpgConditions['prone'] = {
+  desc: 'A restrained creature\'s speed becomes 0, and it can\'t benefit from any bonus to its speed.\nAttack rolls against the creature have advantage, and the creature\'s attack rolls have disadvantage.\nThe creature has disaadvantage on Dexterity saving throws.'
+};
+rpgConditions['stunned'] = {
+  desc: 'A stunned creature is incapacitated (see the condition), can\'t move, and can speak only falteringly.\nThe creature automatically fails Strength and Dexterity saving throws.\nAttack rolls against the creature have advantage.'
+};
+rpgConditions['Unconscious'] = {
+  desc: 'An unconscious creature is **incapacitated**, can\'t move or speak, and is unaware of its surroundings.\nThe creature drops whatever it\'s holding and falls prone.\nThe creature automatically fails Strength and Dexterity saving throws.\nAttack rolls against the creature have advantage.\nAny attack that hits the creature is a critical hit if the attacker is within 5 feet of the creature.'
+};
 exports.conditionList = function() {
 	var rpgConditionsList = new Array();
 	for (var keyIter in rpgConditions) {
-		keyIter.charAt(0).toUpperCase() + keyIter.slice(1);
 		rpgConditionsList.push(keyIter);
   }
 	var rpgConditionsReturn = rpgConditionsList.sort().toString().replace(/,/g, ", ");
