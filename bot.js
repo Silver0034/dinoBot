0852b0a7f8d8a17be6babdbc17e541a4ba07980d
Silver0034 <airraid.mail@gmail.com>
2017-10-08 17:24:42 -0400

    Add files via upload

--- bot.js
+++ bot.js
@@ -507,9 +507,11 @@ commandDictionary['profile'] = {
   error: 'Use the command like this: `profile',
   usage: '**Usage:** `profile',
   doCommand: function(message, key, args) {
-		Jimp.read('./assets.profile.png', Jimp.loadFont(Jimp.FONT_SANS_32_BLACK).then(function (font) {
-    	image.print(font, 10, 10, "Hello world!");
-		}))}
+		message.channel.sendMessage('', {
+			file: './assets.profile.png'
+			return;
+		});
+	}
 };
 /*
 commandDictionary['dex'] = {
