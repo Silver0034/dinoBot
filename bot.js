b571afd8004f8bcbf95ee96b63c588d5bcbc17a4
Silver0034 <airraid.mail@gmail.com>
2017-10-09 21:56:33 -0400

    Add files via upload

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ const MYSQLCRED = tokenReturn.sqlCredentials;
 const emojiDino = '<:sauropod:355738679211327488> ';
 var timedOutUsers = new Array();
 var sqldb = mysql.createConnection(MYSQLCRED);
-var attatchment = new String;
+var attatchment = '';
 
 //global functions
 //puts user in timeout
