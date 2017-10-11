99274baaa1f3191c2d7903bbcb183ac46cf0dba0
Silver0034 <airraid.mail@gmail.com>
2017-10-11 17:46:37 -0400

    Add files via upload

--- commandFunctions/jimp.js
+++ commandFunctions/jimp.js
@@ -1,30 +1,31 @@
 //jimp
-exports.profile = function(Jimp,jimpUserCardBlank,
-								 userBackground,
-								 jimpFontMS16pt500Black,
-								 jimpFontMS18pt900White,
-								 jimpFontMS24pt100Black,
-								 jimpFontMS24pt700Black,
-								 jimpFontMS36ptTitleBlack,
-								 jimpFontMS36ptTitleWhite,
-								 jimpFontMS53ptTitleBlack,
-								 message,
-								 key,
-								 args) {
-	Jimp.read('./assets/profile.png', function (err, image) {
+exports.profile = function(Jimp,
+													 jimpUserCardBlank,
+													 userBackground,
+													 jimpFontMS16pt500Black,
+													 jimpFontMS18pt900White,
+													 jimpFontMS24pt100Black,
+													 jimpFontMS24pt700Black,
+													 jimpFontMS36ptTitleBlack,
+													 jimpFontMS36ptTitleWhite,
+													 jimpFontMS53ptTitleBlack,
+													 message,
+													 key,
+													 args) {
+	var image = 'assets/profile.png';
+	Jimp.read(image, function (err, image) {
 		if (err) throw err;
 			//517 is the full xp bar. Pick a number between 1 and 517
 			var xp = new Jimp(517, 11, 0x64FFDAFF, function (err, xp) {
 				attachment = './assets/userProfile.' + image.getExtension();
-				image
-					.composite(userBackground, 0, 0)
+				image.composite(userBackground, 0, 0)
 					.print(jimpFontMS36ptTitleWhite, 280, 146, message.author.username, 500)
 					.composite(xp, 247, 464)
 					.write(attachment, function() {
-					message.channel.send(emojiDino + ' ' + message.author.username + '\'s Profile', {
-						file: attachment
-					});
-					message.channel.stopTyping();
+						message.channel.send(emojiDino + ' ' + message.author.username + '\'s Profile', {
+							file: attachment
+						});
+						message.channel.stopTyping();
 					});
 			});
 	});
