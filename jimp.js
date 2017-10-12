a5a602c0c57b509fb54fbd5fb2beb43b107396c5
Silver0034 <airraid.mail@gmail.com>
2017-10-11 23:11:48 -0400

    Add files via upload

--- jimp.js
+++ jimp.js
@@ -19,41 +19,51 @@ exports.profile = function(Jimp,
 	*/
 	//Assembling the picture
 	var userCard = new Jimp(800, 500, 0x000000, function (err, image) {
-		.write(attachment, function() {
-			message.channel.send(emojiDino + ' Step 1:', {
-		   file: attachment
-		  });
-		}
 		//set where the picture will be saved at the end
 		attachment = './assets/UserProfile.png';
 		if (err) throw err;
-		message.channel.send(emojiDino + 'Step 1:', {
-			file: attachment
-		});
 		// Put Plate over Background
-		Jimp.read('assets/userBackground/default.png', function (err, image) {
+		Jimp.read('./assets/userBackground/default.png', function (err, background) {
 			if (err) throw err;
-			image
-			.composite(image, 0, 0);
-			//TODO: Resize background to fit width and height
 			Jimp.read('./assets/profile.png', function (err, plate) {
-				image
-				.composite(image, 0, 0);
 				//XP BAR in image
 				//XP Bar Max Width = 517px
 				//TODO: Make Width Represent percentage to next level
 				var xp = new Jimp(517, 11, 0x64FFDAFF, function (err, xp) {
-					xp.composite(xp, 247, 464);
-					//Write Username
-					Jimp.loadFont('./assets/fonts/museo-sans-title-36pt-white.fnt').then(function (jimpFontMS36ptTitleWhite) {
-						image.print(jimpFontMS36ptTitleWhite, 280, 146, message.author.username, 30)
-						//Put this at the end of the final Jimp process
-						.write(attachment, function() {
-							message.channel.send(emojiDino + ' ' + message.author.username + '\'s Profile', {
-								file: attachment
+					//Avatar Mask
+					Jimp.read('./assets/avatarCircleMast.png', function (err, avatarMask) {
+						//Avatar
+						Jimp.read(message.author.avatarURL, function (err, avatar) {
+							//Loads Fonts
+							Jimp.loadFont('./assets/fonts/museo-sans-500-16pt-black.fnt').then(function (jimpFontMS16pt500Black) {
+								Jimp.loadFont('./assets/fonts/museo-sans-900-18pt-white.fnt').then(function (jimpFontMS18pt900White) {
+									Jimp.loadFont('./assets/fonts/museo-sans-100-24pt-black.fnt').then(function (jimpFontMS24pt100Black) {
+										Jimp.loadFont('./assets/fonts/museo-sans-700-24pt-black.fnt').then(function (jimpFontMS24pt700Black) {
+											Jimp.loadFont('./assets/fonts/museo-sans-title-36pt-black.fnt').then(function (jimpFontMS36ptTitleBlack) {
+												Jimp.loadFont('./assets/fonts/museo-sans-title-36pt-white.fnt').then(function (jimpFontMS36ptTitleWhite) {
+													Jimp.loadFont('./assets/fonts/museo-sans-title-53pt-black.fnt').then(function (jimpFontMS53ptTitleBlack) {
+														//Assemble Avatar
+														avatar.cover(193, 193);
+														//Assemble Image
+														image.composite(background, 0, 0)
+														.composite(plate, 0, 0)
+														.composite(avatar, 0, 0)
+														.composite(xp, 247, 464)
+														.print(jimpFontMS36ptTitleWhite, 280, 146, message.author.username, 30)
+														.write(attachment, function() {
+															message.channel.send(emojiDino + ' ' + message.author.username + '\'s Profile', {
+																file: attachment
+															});
+															message.channel.stopTyping();
+															return;
+														});
+													});
+												});
+											});
+										});
+									});	
+								});
 							});
-							message.channel.stopTyping();
-							return;
 						});
 					});
 				});
