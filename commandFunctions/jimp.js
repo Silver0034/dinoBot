cc7723c45cc806954c9795de6224fc0f4a9e505d
Silver0034 <airraid.mail@gmail.com>
2017-10-11 20:56:35 -0400

    Add files via upload

--- commandFunctions/jimp.js
+++ commandFunctions/jimp.js
@@ -1,20 +1,20 @@
 //jimp
-//jimp fonts
-const jimpFontMS16pt500Black = new Jimp.loadFont('./assets/fonts/museo-sans-500-16pt-black.fnt');
-const jimpFontMS18pt900White = new Jimp.loadFont('./assets/fonts/museo-sans-900-18pt-white.fnt');
-const jimpFontMS24pt100Black = new Jimp.loadFont('./assets/fonts/museo-sans-100-24pt-black.fnt');
-const jimpFontMS24pt700Black = new Jimp.loadFont('./assets/fonts/museo-sans-700-24pt-black.fnt');
-const jimpFontMS36ptTitleBlack = new Jimp.loadFont('./assets/fonts/museo-sans-title-36pt-black.fnt');
-const jimpFontMS36ptTitleWhite = new Jimp.loadFont('./assets/fonts/museo-sans-title-36pt-white.fnt');
-const jimpFontMS53ptTitleBlack = new Jimp.loadFont('./assets/fonts/museo-sans-title-53pt-black.fnt');
-//jimp const images
-const jimpUserCardBlank = Jimp.read('./assets/profile.png');
-
 exports.profile = function(Jimp,
 													 message,
 													 key,
 													 args,
 													 emojiDino) {
+	//jimp fonts
+	const jimpFontMS16pt500Black = new Jimp.loadFont('./assets/fonts/museo-sans-500-16pt-black.fnt');
+	const jimpFontMS18pt900White = new Jimp.loadFont('./assets/fonts/museo-sans-900-18pt-white.fnt');
+	const jimpFontMS24pt100Black = new Jimp.loadFont('./assets/fonts/museo-sans-100-24pt-black.fnt');
+	const jimpFontMS24pt700Black = new Jimp.loadFont('./assets/fonts/museo-sans-700-24pt-black.fnt');
+	const jimpFontMS36ptTitleBlack = new Jimp.loadFont('./assets/fonts/museo-sans-title-36pt-black.fnt');
+	const jimpFontMS36ptTitleWhite = new Jimp.loadFont('./assets/fonts/museo-sans-title-36pt-white.fnt');
+	const jimpFontMS53ptTitleBlack = new Jimp.loadFont('./assets/fonts/museo-sans-title-53pt-black.fnt');
+	//jimp const images
+	const jimpUserCardBlank = Jimp.read('./assets/profile.png');
+	//Assembling the picture
 	Jimp.read('assets/profile.png', function (err, image) {
 		if (err) throw err;
 			//XP Bar in Image
