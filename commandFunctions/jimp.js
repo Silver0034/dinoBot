bc5353fc1ae9c39eeb56470f4d2724aca2dd2e89
Silver0034 <airraid.mail@gmail.com>
2017-10-16 22:51:15 -0400

    Add files via upload

--- commandFunctions/jimp.js
+++ commandFunctions/jimp.js
@@ -1,5 +1,5 @@
 //jimp
-exports.profile = function(JIMP,
+exports.profile = function(jimp,
 													 message,
 													 key,
 													 args,
@@ -12,47 +12,47 @@ exports.profile = function(JIMP,
 		console.log('2');
 		console.log(userBackground);
 		//Assembling the picture
-		var userCard = new JIMP(800, 500, 0x000000, function (err, image) {
+		var userCard = new jimp(800, 500, 0x000000, function (err, image) {
 			console.log('2.1');
 			//set where the picture will be saved at the end
 			attachment = './assets/UserProfile.png';
 			if (err) throw err;
 			console.log('2.2');
 			// Put Plate over Background
-			JIMP.read(userBackground, function (err, background) {
+			jimp.read(userBackground, function (err, background) {
 				console.log('3');
 				background.cover(800, 198)
 				.blur(1)
 				.brightness(-0.2);
 				if (err) throw err;
-				JIMP.read('./assets/profile.png', function (err, plate) {
+				jimp.read('./assets/profile.png', function (err, plate) {
 					console.log('4');
 					//XP BAR in image
 					//XP Bar Max Width = 517px
 					//TODO: Make Width Represent percentage to next level
-					var xp = new JIMP(517, 11, 0x64FFDAFF, function (err, xp) {
+					var xp = new jimp(517, 11, 0x64FFDAFF, function (err, xp) {
 						//Avatar Mask
 						//Set default if null
 						var avatarPath = './assets/avatarDefault.png';
 						if (message.author.avatarURL != null) {
 							avatarPath = message.author.avatarURL;
 						}
-						JIMP.read('./assets/avatarCircleMask.png', function (err, mask) {
+						jimp.read('./assets/avatarCircleMask.png', function (err, mask) {
 							console.log('5');
 							//Avatar
-							JIMP.read(avatarPath, function (err, avatar) {
+							jimp.read(avatarPath, function (err, avatar) {
 								//Assemble Avatar
 								avatar.cover(193, 193)
 								.mask(mask, 0, 0);
 								//Loads Fonts
 								console.log('6');
-								JIMP.loadFont('./assets/fonts/museo-sans-500-16pt-black.fnt').then(function (jimpFontMS16pt500Black) {
-									JIMP.loadFont('./assets/fonts/museo-sans-900-18pt-white.fnt').then(function (jimpFontMS18pt900White) {
-										JIMP.loadFont('./assets/fonts/museo-sans-100-24pt-black.fnt').then(function (jimpFontMS24pt100Black) {
-											JIMP.loadFont('./assets/fonts/museo-sans-700-24pt-black.fnt').then(function (jimpFontMS24pt700Black) {
-												JIMP.loadFont('./assets/fonts/museo-sans-title-36pt-black.fnt').then(function (jimpFontMS36ptTitleBlack) {
-													JIMP.loadFont('./assets/fonts/museo-sans-title-36pt-white.fnt').then(function (jimpFontMS36ptTitleWhite) {
-														JIMP.loadFont('./assets/fonts/museo-sans-title-53pt-black.fnt').then(function (jimpFontMS53ptTitleBlack) {
+								jimp.loadFont('./assets/fonts/museo-sans-500-16pt-black.fnt').then(function (jimpFontMS16pt500Black) {
+									jimp.loadFont('./assets/fonts/museo-sans-900-18pt-white.fnt').then(function (jimpFontMS18pt900White) {
+										jimp.loadFont('./assets/fonts/museo-sans-100-24pt-black.fnt').then(function (jimpFontMS24pt100Black) {
+											jimp.loadFont('./assets/fonts/museo-sans-700-24pt-black.fnt').then(function (jimpFontMS24pt700Black) {
+												jimp.loadFont('./assets/fonts/museo-sans-title-36pt-black.fnt').then(function (jimpFontMS36ptTitleBlack) {
+													jimp.loadFont('./assets/fonts/museo-sans-title-36pt-white.fnt').then(function (jimpFontMS36ptTitleWhite) {
+														jimp.loadFont('./assets/fonts/museo-sans-title-53pt-black.fnt').then(function (jimpFontMS53ptTitleBlack) {
 															//Assemble Image
 															console.log('7');
 															image.composite(background, 0, 0)
