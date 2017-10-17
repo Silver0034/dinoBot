ca4d0c7c827257b7ea76355958cc04a989914800
Silver0034 <airraid.mail@gmail.com>
2017-10-16 21:56:15 -0400

    Add files via upload

--- bot.js
+++ bot.js
@@ -560,10 +560,9 @@ commandDictionary['profile'] = {
             var imageInputURL = '';
             var imageUrlSplit = args[1].split(':');
             if (imageUrlSplit[0] == 'https') {
-							imageUrlSplit[0] = 'http';
+							imageUrlSplit[0] = 'http:';
 						}
 						imageInputURL = imageUrlSplit.join('');
-						console.log(imageInputURL);
 						//what to do if link is added
 						if (VALIDURL.isUri(imageInputURL)) {
 							//check if image is a png
