6ec63202605950c3aa9ac197f527204489cc68c9
Silver0034 <airraid.mail@gmail.com>
2017-11-03 18:53:37 -0400

    Add files via upload

--- bot.js
+++ bot.js
@@ -847,6 +847,43 @@ commandDictionary['name'] = {
         return;
       case 'toggle':
         //Switch between two usernames
+        //Pull toggle number from database
+        sqldb.query("SELECT * FROM user WHERE userID = " + message.author.id, function (err, results, fields) {
+		      var nicknameToggleState = results[0].nicknameToggle;
+          var nicknameOne = results[0].nicknameOne;
+          var nicknameTwo = results[0].nicknameTwo;
+          if (nicknameToggleState = 0) {
+            if (message.guild) {
+              //check bot has permissions to change nicknames
+              if (message.guild.members.get(bot.user.id).hasPermission("MANAGE_NICKNAMES") && message.guild.members.get(bot.user.id).hasPermission("CHANGE_NICKNAME")) {
+                //change nickname
+                message.guild.members.get(bot.user.id).setNickname(nicknameOne);
+              } else {
+                //If does not have permission
+                message.channel.sendMessage(responseHead(message, key) + 'I\'m sorry, I do not have permissions to manage nicknames on this server.');
+              }  
+            } else {
+              //not in a server (in a DM)
+              message.channel.sendMessage(responseHead(message, key) + 'I\'m sorry, I can only change your nickname in a server.');
+            }
+          }
+          if (nicknameToggleState = 1) {
+            if (message.guild) {
+              //check bot has permissions to change nicknames
+              if (message.guild.members.get(bot.user.id).hasPermission("MANAGE_NICKNAMES") && message.guild.members.get(bot.user.id).hasPermission("CHANGE_NICKNAME")) {
+                //change nickname
+                message.guild.members.get(bot.user.id).setNickname(nicknameTwo);
+              } else {
+                //If does not have permission
+                message.channel.sendMessage(responseHead(message, key) + 'I\'m sorry, I do not have permissions to manage nicknames on this server.');
+              }  
+            } else {
+              //not in a server (in a DM)
+              message.channel.sendMessage(responseHead(message, key) + 'I\'m sorry, I can only change your nickname in a server.');
+            }
+          }
+        }
+        //
         return;
     }
     message.channel.send(responseHead(message, key) + 'Please use the command as follows: `name [set OR toggle]');
