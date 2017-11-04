ee074b0ac1863b1d08934d03773101269267f5d2
Silver0034 <airraid.mail@gmail.com>
2017-11-03 21:25:58 -0400

    Add files via upload

--- bot.js
+++ bot.js
@@ -649,7 +649,13 @@ commandDictionary['rpg'] = {
             .setImage("https://media-waterdeep.cursecdn.com/avatars/thumbnails/0/16/315/315/636238882493439723.jpeg")
             .setThumbnail("https://cdn.discordapp.com/attachments/358264614200279050/376058047614943232/dnd-beyond-logo.png")
             .setURL("https://www.dndbeyond.com/monsters/adult-blue-dragon")
-            .addField("This is a field title, it can hold 256 characters")
+            .addField("__**Abilities**__", "|")
+            .addField("<:strength:376009689864994820> **STR**", abilityScoreArray[0] + " " + abilityModifierArray[0], true)
+            .addField("<:dexterity:376009689348964352> **DEX**", abilityScoreArray[1] + " " + abilityModifierArray[1], true)
+            .addField("<:constitution:376009689214877707> **CON**", abilityScoreArray[2] + " " + abilityModifierArray[2], true)
+            .addField("<:intelligence:376009689445564426> **INT**", abilityScoreArray[3] + " " + abilityModifierArray[3], true)
+            .addField("<:wisdom:376009690796261377> **WIS**", abilityScoreArray[4] + " " + abilityModifierArray[4], true)
+            .addField("<:charisma:376009688988516353> **CHA**", abilityScoreArray[5] + " " + abilityModifierArray[5], true)
             .addBlankField(true)
             .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
 
