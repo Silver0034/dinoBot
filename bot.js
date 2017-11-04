5d5dc5eb56faaf39ce759dc4a4bd611a28ae154d
Silver0034 <airraid.mail@gmail.com>
2017-11-03 23:10:42 -0400

    Add files via upload

--- bot.js
+++ bot.js
@@ -667,10 +667,12 @@ commandDictionary['rpg'] = {
                       , false)
             //Secondary Information
             .addField("__**Stats**__",
-                        "**" + statsTitle[6] + "**: " + quickPrimaryArray[0] + " " + quickSecondaryArray[0] +
+                        "**" + statsTitle[6] + "**: " + quickPrimaryArray[0] + " " + quickSecondaryArray[0]
+                      /*
                       + "**" + statsTitle[7] + "**: " + quickPrimaryArray[1] + " " + quickSecondaryArray[1] +
                       + "**" + statsTitle[8] + "**: " + quickPrimaryArray[2] + " " + quickSecondaryArray[2] +
                       + "**" + statsTitle[9] + "**: " + quickPrimaryArray[3] + " " + quickSecondaryArray[3]
+                      */
                      )
             .addField("__**Secondary Information**__", "Miscellaneous stats for the " + page["title"])
             .addField("**Challenge**", quickPrimaryArray[0] + " " + quickSecondaryArray[0], true)
