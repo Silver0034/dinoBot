cad6fa6567796e4f80465a460c78a2ec305b99d5
Silver0034 <airraid.mail@gmail.com>
2017-11-04 10:59:57 -0400

    Add files via upload

--- bot.js
+++ bot.js
@@ -629,9 +629,9 @@ commandDictionary['rpg'] = {
           for (n = 1; k < actionsValueArray.length; n++) {  
             o = featsValueArray.length - 1;
             actionsValueString += "**" + strongArray[o].replace('.', ':') + '** ' + actionsValueArray[n] + '\n';
-            message.channel.send("In Actions For-Loop " + m + ": " + actionsLoopPlaceholder);
+            message.channel.send("In Actions For-Loop " + m + ": " + actionsValueString);
           }
-          embed.addField("__**Actions**__", actionsValueString, true);
+          //embed.addField("__**Actions**__", actionsValueString, true);
           /*
           console.log('--------------------------------------------------------------\n' + moreInfoContent);
           message.channel.send({embed});
