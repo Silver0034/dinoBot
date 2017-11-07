a89050bd6a4a22d83c343625c02876064ab9570c
Silver0034 <airraid.mail@gmail.com>
2017-11-06 21:52:47 -0500

    Add files via upload

--- bot.js
+++ bot.js
@@ -478,7 +478,7 @@ commandDictionary['rpg'] = {
   doCommand: function(message, key, args) {
   	switch(args[0]) { 
       case 'scrape':
-        var scrapeURL = "https://www.dndbeyond.com/monsters/aboleth";        
+        var scrapeURL = "https://www.dndbeyond.com/monsters/axe-beak";        
         SCRAPEIT(scrapeURL, {
           
           title: ".monster-name",
