a94216d856ce7a839a75beb5719cfa4bbb302f54
Silver0034 <airraid.mail@gmail.com>
2017-11-08 20:12:14 -0500

    Add files via upload

--- bot.js
+++ bot.js
@@ -930,19 +930,22 @@ commandDictionary['name'] = {
                 //if error make log
                 message.member.setNickname(nickname);
                 message.channel.send(responseHead(message, key) + 'Your nickname has been changed to ' + nickname);
+                return;
               } else {
                 //If does not have permission
                 message.channel.send(responseHead(message, key) + 'I\'m sorry, I do not have permissions to manage nicknames on this server.');
+                return;
               }
             } else {
               //not in a server (in a DM)
               message.channel.send(responseHead(message, key) + 'I\'m sorry, I can only change your nickname in a server.');
+              return;
             }
           } catch(err) {
             console.log(error);
             message.channel.send(responseHead(message, key) + 'I\'m sorry, I can only change the nickname of users with a lower rank than me');
+            return;
           }
-          return;
         });
         
     }
