21698b448c8fb04d891e2999ec740d0b0e1aba46
Silver0034 <airraid.mail@gmail.com>
2017-11-10 23:49:41 -0500

    Add files via upload

---
+++ commandFunctions/nameGen.js
@@ -0,0 +1,184 @@
+//NameGen.j
+
+var name = new Object();
+name['human'] = {
+  maleOne: [
+    "",
+    "",
+    "",
+    "",
+    "a",
+    "be",
+    "de",
+    "el",
+    "fa",
+    "jo",
+    "ki",
+    "la",
+    "ma",
+    "na",
+    "o",
+    "pa",
+    "re",
+    "si",
+    "ta",
+    "va"
+  ],
+  maleTwo: [
+    "bar",
+    "ched",
+    "dell",
+    "far",
+    "gran",
+    "hal",
+    "jen",
+    "kel",
+    "lim",
+    "mor",
+    "net",
+    "penn",
+    "quil",
+    "rond",
+    "sark",
+    "shen",
+    "tur",
+    "vash",
+    "yor",
+    "zen"
+  ],
+  maleThree: [
+    "",
+    "a",
+    "ac",
+    "ai",
+    "al",
+    "am",
+    "an",
+    "ar",
+    "ea",
+    "el",
+    "er",
+    "ess",
+    "ett",
+    "ic",
+    "id",
+    "il",
+    "in",
+    "is",
+    "or",
+    "us"
+  ],
+  femaleOne: [
+    "",
+    "",
+    "",
+    "",
+    "a",
+    "bi",
+    "di",
+    "el",
+    "fi",
+    "jo",
+    "ki",
+    "la",
+    "mo",
+    "no",
+    "o",
+    "po",
+    "ro",
+    "si",
+    "ti",
+    "vi"
+  ],
+  femaleTwo: [
+    "or",
+    "ph",
+    "fr",
+    "lv",
+    "m",
+    "rl",
+    "sh",
+    "se",
+    "in",
+    "et",
+    "te",
+    "nn",
+    "ll",
+    "li",
+    "ri",
+    "ss",
+    "ea",
+    "el",
+    "or",
+    "ic"
+  ],
+  femaleThree: [
+    "a",
+    "ia",
+    "thia",
+    "ego",
+    "da",
+    "ene",
+    "ih",
+    "ith",
+    "hi",
+    "ni",
+    "mi",
+    "en",
+    "thi",
+    "th",
+    "il",
+    "la",
+    "ra",
+    "ett",
+    "ss",
+    "y"
+  ]
+}
+
+exports.generate = function(message, key, args) {
+  var nameReturn = '';
+  
+  if (name[args[0]].maleOne == null || name[args[0]].maleOne == undefined) {
+    return 'Please specify a valid race';
+  }
+  
+  if (args == 'female') {
+    //if female
+    //pick name part 1
+    var nameFirst = Math.floor((Math.random() * name[args[0]].maleOne.length));
+    //add name part one to return
+    nameReturn += nameFirst;    
+    //pick name part 2
+	  var nameSecond = Math.floor((Math.random() * name[args[0]].maleTwo.length)); 
+    //add name part 2 to return
+    nameReturn += nameSecond; 
+    //pick name part 3
+    var nameThird = Math.floor((Math.random() * name[args[0]].maleThree.length));
+    //add name part 3 to return
+    nameReturn += nameThird; 
+    //return name
+	  return nameReturn.charAt(0).toUpperCase() + nameReturn.slice(1);
+    
+  } else if(args == 'male' || args == undefined || args == null) {
+    //if male or not specified
+    //pick name part 1
+    var nameFirst = Math.floor((Math.random() * name[args[0]].maleOne.length));
+    //add name part one to return
+    nameReturn += nameFirst;    
+    //pick name part 2
+	  var nameSecond = Math.floor((Math.random() * name[args[0]].maleTwo.length)); 
+    //add name part 2 to return
+    nameReturn += nameSecond; 
+    //pick name part 3
+    var nameThird = Math.floor((Math.random() * name[args[0]].maleThree.length));
+    //add name part 3 to return
+    nameReturn += nameThird; 
+    //return name
+	  return nameReturn.charAt(0).toUpperCase() + nameReturn.slice(1);
+  } else {
+    //return error if not male, female, or empty
+    return 'Please specify either male, female, or leave blank';
+  }
+  
+}
