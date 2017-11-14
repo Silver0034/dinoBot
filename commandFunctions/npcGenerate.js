584a431ec40ad98bbe4de14d34e9934aee545645
Silver0034 <airraid.mail@gmail.com>
2017-11-13 20:12:08 -0500

    Add files via upload

--- commandFunctions/npcGenerate.js
+++ commandFunctions/npcGenerate.js
@@ -651,6 +651,27 @@ function raceList() {
   return raceList;
 }
 
+//create list of armor
+function armorList() {
+  SCRAPEIT(
+    'https://www.dndbeyond.com/equipment?filter-search=armor&filter-cost-min=&filter-cost-max=&filter-weight-min=&filter-weight-max=',
+    {
+      name: {
+        listItem: ".link"
+      }
+    },
+    (err, page) => {
+      
+      console.log(page.name);
+      var armorListArray = page.name;
+      var armorListSplitNum = armorListArray.length - 9;
+      armorListArray = armorListArray.slice(0, armorListSplitNum);
+      console.log(armorListArray);
+      return armorListArray;
+    }
+  );
+}
+
 //return string of supported races
 exports.raceList = function() {
   return '```' + raceList().sort().toString().replace(/,/g, ", ") + '```';
@@ -700,21 +721,5 @@ exports.nameFemale = function(race) {
 }
 
 exports.armorList = function(SCRAPEIT) {  
-  SCRAPEIT(
-  'https://www.dndbeyond.com/equipment?filter-search=armor&filter-cost-min=&filter-cost-max=&filter-weight-min=&filter-weight-max=',
-    {
-      name: {
-        listItem: ".link"
-      }
-    },
-    (err, page) => {
-      
-      console.log(page.name);
-      var armorListArray = page.name;
-      var armorListSplitNum = armorListArray.length - 9;
-      armorListArray = armorListArray.slice(0, armorListSplitNum);
-      console.log(armorListArray);
-      return armorListArray;
-    }
-  );
+  return armorList().toString();
 }
