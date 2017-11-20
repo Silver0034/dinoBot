18bd5d23d7616e55b346b284b0cdd4f9703d1974
Silver0034 <airraid.mail@gmail.com>
2017-11-19 20:09:09 -0500

    Add files via upload

--- bot.js
+++ bot.js
@@ -1141,7 +1141,7 @@ commandDictionary['npc'] = {
         return;
       }
       
-      console.log('||||| + ' classInfoArray)
+      console.log('||||| + ' + classInfoArray);
       if (classInfoArray != undefined || classInfoArray != null) {
         embed
              .setTitle(setName + ': ' + args[0].charAt(0).toUpperCase() + args[0].slice(1).toLowerCase())
