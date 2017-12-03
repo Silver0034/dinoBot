619650f91ba4c08edebd4fd2492343dc399721bc
Silver0034 <airraid.mail@gmail.com>
2017-12-02 19:50:35 -0500

    Add files via upload

--- bot.js
+++ bot.js
@@ -106,7 +106,6 @@ emoji = {
   cha: '<:charisma:377677013428862978>',
   monster: '<:beholder:386676248434180106>'
 }
-console.log(emoji.monster);
 
 //dictionary for all commands and information
 var commandDictionary = new Object();
@@ -496,7 +495,7 @@ commandDictionary['rps'] = {
     return;
 	}
 };
-commandDictionary['monster'] = {
+commandDictionary['rpg'] = {
   icon: 'https://github.com/Silver0034/dinoBot/blob/master/assets/icons/dnd-beyond-logo.png?raw=true',
   emoji: emoji.monster,
   error: 'Use the command like this: `rpg name character',
@@ -504,225 +503,6 @@ commandDictionary['monster'] = {
   doCommand: function(message, key, args) {
     
   	switch(args[0]) { 
-      case 'monster':
-        if (args[1]) {
-          console.log(args);
-          var scrapeInput = args.join('-').substring(8);
-          console.log(scrapeInput);
-        } else {
-          message.channel.startTyping(); 
-          const embed = new DISCORD.RichEmbed()
-            .setTitle('Incorrect Format')
-            .setAuthor(BOT.user.username, BOT.user.avatarURL)
-            .setColor(0x64FFDA)
-            .setDescription('Use the command like this: ````rpg monster [monster name]```')
-            .setFooter("© 2017 D&D Beyond | Scraped by " + BOT.user.username + '™', "commandDictionary[key].icon")
-            .setImage('https://static-waterdeep.cursecdn.com/1-0-6519-15606/Skins/Waterdeep/images/errors/404.png')
-            .setThumbnail(commandDictionary[key].icon);
-          message.channel.stopTyping();
-          message.channel.send({embed});
-          return;
-        }
-        
-        message.channel.startTyping();          
-        
-        var scrapeURL = "https://www.dndbeyond.com/monsters/";
-        scrapeURL = scrapeURL + scrapeInput;        
-             
-        SCRAPEIT(scrapeURL, {
-          
-          title: ".monster-name",
-          descShort: ".details-item",
-          // Nested list
-          abilityScore: {
-            listItem: ".score"
-          },
-          abilityModifier: {
-            listItem: ".modifier"
-          },
-          quickPrimary: {
-            listItem: ".primary"
-          },
-          quickSecondary: {
-            listItem: ".secondary"
-          },
-          statsTitle: {
-            listItem: ".title",
-          },
-          statsDescription: {
-            listItem: ".description",
-          },
-          strong: {
-            listItem: "strong",
-          },
-          strong: {
-            listItem: "strong",
-          },
-          moreInfoContent: {
-            selector: ".more-info-content",
-            how: "html"
-          },
-          moreInfoPlain: {
-            selector: ".more-info-content"
-          },
-          monsterImage: {
-            selector: ".monster-image",
-            attr: "src"
-          },
-          errorPageTitle: {
-            selector: ".error-page-title",
-            how: "text"
-          },
-        },
-          (err, page) => {
-          //console.log(err);
-          
-          if (page.errorPageTitle == 'Page Not Found') {
-            const embed = new DISCORD.RichEmbed()
-              .setTitle('Monster Not Found')
-              .setAuthor(BOT.user.username, BOT.user.avatarURL)
-              .setColor(0x64FFDA)
-              .setDescription('The Monster you searched for is not on D&D Beyond.')
-              .setFooter("© 2017 D&D Beyond | Scraped by " + BOT.user.username + '™', "commandDictionary[key].icon")
-              .setImage('https://static-waterdeep.cursecdn.com/1-0-6519-15606/Skins/Waterdeep/images/errors/404.png')
-              .setThumbnail(commandDictionary[key].icon);
-            message.channel.stopTyping();
-            message.channel.send({embed});
-            return;
-          }
-          
-          var abilityScoreArray = page["abilityScore"];
-          var abilityModifierArray = page["abilityModifier"];
-          var quickPrimaryArray = page["quickPrimary"];
-          var quickSecondaryArray = page["quickSecondary"];
-          var statsTitle = page["statsTitle"];
-          var statsDescription = page["statsDescription"];
-          var strongArray = page["strong"];
-          var moreInfoContent = page["moreInfoContent"] + "";
-                
-          var proficiencyValue = '';
-          var featsLoopArray = moreInfoContent.split('Actions\r\n');
-          var featsLoopPlaceholder = featsLoopArray[0];
-          var featsValueArray = [];
-          var featsValueString = '';
-          var actionsValueArray = [];
-          var actionsValueString = '';
-          var monsterImageURL = page.monsterImage;
-          
-          
-          if (page.monsterImage == undefined) {
-            const embed = new DISCORD.RichEmbed()
-              .setTitle('Monster Not Available')
-              .setAuthor(BOT.user.username, BOT.user.avatarURL)
-              .setColor(0x64FFDA)
-              .setDescription('I only have acsess to monsters defined by the "basic rules"')
-              .setFooter("© 2017 D&D Beyond | Scraped by " + BOT.user.username + '™', "commandDictionary[key].icon")
-              .setThumbnail(commandDictionary[key].icon);
-            message.channel.stopTyping();
-            message.channel.send({embed});
-            return;
-          }
-          
-          if (page.monsterImage.includes('https:') == false) {
-            monsterImageURL = 'https:' + page.monsterImage;
-          }
-          
-          var quickContent = [];
-          
-          for (q = 0; q < page.quickPrimary.length; q++) {
-            if (page.quickSecondary[q]) {
-              quickContent[q] = page.quickPrimary[q] + ' ' + page.quickSecondary[q];
-            } else {
-              quickContent[q] = page.quickPrimary[q];
-            }
-            
-          }
-          
-          //This is how many fields are defined in the const
-          var fieldCount = 2;
-          const embed = new DISCORD.RichEmbed()
-            .setTitle(page["title"])
-            .setAuthor(BOT.user.username, BOT.user.avatarURL)
-            .setColor(0x64FFDA)
-            .setDescription(page["descShort"])
-            .setFooter("© 2017 D&D Beyond | Scraped by " + BOT.user.username, "commandDictionary[key].icon")
-            .setImage(monsterImageURL)
-            .setThumbnail(commandDictionary[key].icon)
-            .setURL(scrapeURL)
-            //Abilities Section          
-            .addField("__**Abilities**__",
-                      emoji.str + " **" + page.statsTitle[0] + "**: " + page.abilityScore[0] + page.abilityModifier[0] +
-                      "  " + emoij.dex + " **" + page.statsTitle[1] + "**: " + page.abilityScore[1] + page.abilityModifier[1] +
-                      "  " + emoij.con + " **" + page.statsTitle[2] + "**: " + page.abilityScore[2] + page.abilityModifier[2] + '\n' +
-                      emoij.int + " **" + page.statsTitle[3] + "**: " + page.abilityScore[3] + page.abilityModifier[3] +
-                      "  " + emoij.wis + " **" + page.statsTitle[4] + "**: " + page.abilityScore[4] + page.abilityModifier[4] +
-                      "  " + emoij.cha + " **" + page.statsTitle[5] + "**: " + page.abilityScore[5] + page.abilityModifier[5]
-                      , false)
-            //Secondary Information
-            .addField("__**Secondary Stats**__",
-                     "**" + page.statsTitle[6] + "**: " + quickContent[0] + "\n" +
-                     "**" + page.statsTitle[7] + "**: " + quickContent[1] + "\n" +
-                     "**" + page.statsTitle[8] + "**: " + quickContent[2] + "\n" +
-                     "**" + page.statsTitle[9] + "**: " + quickContent[3]
-                     , false);
-          
-          //Proficiency Fields
-          for (i = 0; i < page.statsDescription.length; i++) { 
-            z = i + 10;
-            proficiencyValue += '**' + page.statsTitle[z] + '**: ' + page.statsDescription[i] + "\n"
-          }
-          embed.addField("__**Proficiencies**__", proficiencyValue, false);
-          fieldCount++;
-          
-          
-          //Handles page.moreInfoContent
-          var $ = CHEERIO.load(page.moreInfoContent);
-          var paragraph = '';
-          var lineArray = [];
-          var lineSections = [];
-          loopParagraph:
-            for (i = 0; i < $('p').length; i++) {
-
-              paragraph = $('p').eq(i).html();
-
-              //split each paragraph by line breaks
-              lineArray = paragraph.split('<br>');
-              //for each line in the paragraph
-              loopLine:
-                for (j = 0; j < lineArray.length; j++) {
-                  //check field number
-                  if (fieldCount > 24) { 
-                  embed.addField('More at D&D Beyond', 'There is more to this monster. Go to the link above for all of its information.', false);
-                  fieldCount++;
-                  break loopParagraph;
-                  }
-                  if (lineArray[j].includes('</strong>')) {
-                    lineSections = lineArray[j].split('</strong>');
-                    lineSections[0] = lineSections[0].replace('<strong>', '**').replace('.', ':**');
-                    lineSections[1] = '<div class=cheerioLoad>' + lineSections[1] + '</div>';
-                    var lineSectionsCheerio = CHEERIO.load(lineSections[1]);
-                    lineSections[1] = lineSectionsCheerio('.cheerioLoad').text();
-                    lineSections[0] = '<div class=cheerioLoad>' + lineSections[0] + '</div>';
-                    lineSectionsCheerio = CHEERIO.load(lineSections[0]);
-                    lineSections[0] = lineSectionsCheerio('.cheerioLoad').text();
-                    //check to make sure it isn't too long
-                    if (lineSections[0].length > 1024) {
-                      lineSections[1] = lineSections[1].substring(1023) + '…';
-                    }
-                    //make sure nothing went wrong
-                    if (lineSections.length == 2) {
-                      //Create Field
-                      embed.addField(lineSections[0], lineSections[1], false);
-                      fieldCount++; 
-                    } 
-                  }             
-                }
-          }
-          message.channel.stopTyping();
-          message.channel.send({embed});
-          return;
-        });   
-				return;  
 			case 'name':
         message.channel.send(responseHead(message, key) + RPG.name());
 				return;
@@ -1189,6 +969,232 @@ commandDictionary['npc'] = {
     return;
   }
 };
+commandDictionary['monster'] = {
+  timeout: 0,
+  icon: 'https://github.com/Silver0034/dinoBot/blob/master/assets/icons/npcIcon.png?raw=true',
+	emoji: ':man_dancing: ',
+  error: 'Use the command like this: `monster [monster name]',
+  usage: '**Usage:** `npc [class] [race] [gender]',
+  doCommand: function(message, key, args, embedFooter) {
+    if (args[0]) {
+      console.log(args);
+      var scrapeInput = args.join('-').substring(8);
+      console.log(scrapeInput);
+    } else {
+      message.channel.startTyping(); 
+      const embed = new DISCORD.RichEmbed()
+        .setTitle('Incorrect Format')
+        .setAuthor(BOT.user.username, BOT.user.avatarURL)
+        .setColor(0x64FFDA)
+        .setDescription('Use the command like this: ````rpg monster [monster name]```')
+        .setFooter("© 2017 D&D Beyond | Scraped by " + BOT.user.username + '™', "commandDictionary[key].icon")
+        .setImage('https://static-waterdeep.cursecdn.com/1-0-6519-15606/Skins/Waterdeep/images/errors/404.png')
+        .setThumbnail(commandDictionary[key].icon);
+      message.channel.stopTyping();
+      message.channel.send({embed});
+      return;
+    }
+
+    message.channel.startTyping();          
+
+    var scrapeURL = "https://www.dndbeyond.com/monsters/";
+    scrapeURL = scrapeURL + scrapeInput;        
+
+    SCRAPEIT(scrapeURL, {
+
+      title: ".monster-name",
+      descShort: ".details-item",
+      // Nested list
+      abilityScore: {
+        listItem: ".score"
+      },
+      abilityModifier: {
+        listItem: ".modifier"
+      },
+      quickPrimary: {
+        listItem: ".primary"
+      },
+      quickSecondary: {
+        listItem: ".secondary"
+      },
+      statsTitle: {
+        listItem: ".title",
+      },
+      statsDescription: {
+        listItem: ".description",
+      },
+      strong: {
+        listItem: "strong",
+      },
+      strong: {
+        listItem: "strong",
+      },
+      moreInfoContent: {
+        selector: ".more-info-content",
+        how: "html"
+      },
+      moreInfoPlain: {
+        selector: ".more-info-content"
+      },
+      monsterImage: {
+        selector: ".monster-image",
+        attr: "src"
+      },
+      errorPageTitle: {
+        selector: ".error-page-title",
+        how: "text"
+      },
+    },
+      (err, page) => {
+      //console.log(err);
+
+      if (page.errorPageTitle == 'Page Not Found') {
+        const embed = new DISCORD.RichEmbed()
+          .setTitle('Monster Not Found')
+          .setAuthor(BOT.user.username, BOT.user.avatarURL)
+          .setColor(0x64FFDA)
+          .setDescription('The Monster you searched for is not on D&D Beyond.')
+          .setFooter("© 2017 D&D Beyond | Scraped by " + BOT.user.username + '™', "commandDictionary[key].icon")
+          .setImage('https://static-waterdeep.cursecdn.com/1-0-6519-15606/Skins/Waterdeep/images/errors/404.png')
+          .setThumbnail(commandDictionary[key].icon);
+        message.channel.stopTyping();
+        message.channel.send({embed});
+        return;
+      }
+
+      var abilityScoreArray = page["abilityScore"];
+      var abilityModifierArray = page["abilityModifier"];
+      var quickPrimaryArray = page["quickPrimary"];
+      var quickSecondaryArray = page["quickSecondary"];
+      var statsTitle = page["statsTitle"];
+      var statsDescription = page["statsDescription"];
+      var strongArray = page["strong"];
+      var moreInfoContent = page["moreInfoContent"] + "";
+
+      var proficiencyValue = '';
+      var featsLoopArray = moreInfoContent.split('Actions\r\n');
+      var featsLoopPlaceholder = featsLoopArray[0];
+      var featsValueArray = [];
+      var featsValueString = '';
+      var actionsValueArray = [];
+      var actionsValueString = '';
+      var monsterImageURL = page.monsterImage;
+
+
+      if (page.monsterImage == undefined) {
+        const embed = new DISCORD.RichEmbed()
+          .setTitle('Monster Not Available')
+          .setAuthor(BOT.user.username, BOT.user.avatarURL)
+          .setColor(0x64FFDA)
+          .setDescription('I only have acsess to monsters defined by the "basic rules"')
+          .setFooter("© 2017 D&D Beyond | Scraped by " + BOT.user.username + '™', "commandDictionary[key].icon")
+          .setThumbnail(commandDictionary[key].icon);
+        message.channel.stopTyping();
+        message.channel.send({embed});
+        return;
+      }
+
+      if (page.monsterImage.includes('https:') == false) {
+        monsterImageURL = 'https:' + page.monsterImage;
+      }
+
+      var quickContent = [];
+
+      for (q = 0; q < page.quickPrimary.length; q++) {
+        if (page.quickSecondary[q]) {
+          quickContent[q] = page.quickPrimary[q] + ' ' + page.quickSecondary[q];
+        } else {
+          quickContent[q] = page.quickPrimary[q];
+        }
+
+      }
+
+      //This is how many fields are defined in the const
+      var fieldCount = 2;
+      const embed = new DISCORD.RichEmbed()
+        .setTitle(page["title"])
+        .setAuthor(BOT.user.username, BOT.user.avatarURL)
+        .setColor(0x64FFDA)
+        .setDescription(page["descShort"])
+        .setFooter("© 2017 D&D Beyond | Scraped by " + BOT.user.username, "commandDictionary[key].icon")
+        .setImage(monsterImageURL)
+        .setThumbnail(commandDictionary[key].icon)
+        .setURL(scrapeURL)
+        //Abilities Section          
+        .addField("__**Abilities**__",
+                  emoji.str + " **" + page.statsTitle[0] + "**: " + page.abilityScore[0] + page.abilityModifier[0] +
+                  "  " + emoij.dex + " **" + page.statsTitle[1] + "**: " + page.abilityScore[1] + page.abilityModifier[1] +
+                  "  " + emoij.con + " **" + page.statsTitle[2] + "**: " + page.abilityScore[2] + page.abilityModifier[2] + '\n' +
+                  emoij.int + " **" + page.statsTitle[3] + "**: " + page.abilityScore[3] + page.abilityModifier[3] +
+                  "  " + emoij.wis + " **" + page.statsTitle[4] + "**: " + page.abilityScore[4] + page.abilityModifier[4] +
+                  "  " + emoij.cha + " **" + page.statsTitle[5] + "**: " + page.abilityScore[5] + page.abilityModifier[5]
+                  , false)
+        //Secondary Information
+        .addField("__**Secondary Stats**__",
+                 "**" + page.statsTitle[6] + "**: " + quickContent[0] + "\n" +
+                 "**" + page.statsTitle[7] + "**: " + quickContent[1] + "\n" +
+                 "**" + page.statsTitle[8] + "**: " + quickContent[2] + "\n" +
+                 "**" + page.statsTitle[9] + "**: " + quickContent[3]
+                 , false);
+
+      //Proficiency Fields
+      for (i = 0; i < page.statsDescription.length; i++) { 
+        z = i + 10;
+        proficiencyValue += '**' + page.statsTitle[z] + '**: ' + page.statsDescription[i] + "\n"
+      }
+      embed.addField("__**Proficiencies**__", proficiencyValue, false);
+      fieldCount++;
+
+
+      //Handles page.moreInfoContent
+      var $ = CHEERIO.load(page.moreInfoContent);
+      var paragraph = '';
+      var lineArray = [];
+      var lineSections = [];
+      loopParagraph:
+        for (i = 0; i < $('p').length; i++) {
+
+          paragraph = $('p').eq(i).html();
+
+          //split each paragraph by line breaks
+          lineArray = paragraph.split('<br>');
+          //for each line in the paragraph
+          loopLine:
+            for (j = 0; j < lineArray.length; j++) {
+              //check field number
+              if (fieldCount > 24) { 
+              embed.addField('More at D&D Beyond', 'There is more to this monster. Go to the link above for all of its information.', false);
+              fieldCount++;
+              break loopParagraph;
+              }
+              if (lineArray[j].includes('</strong>')) {
+                lineSections = lineArray[j].split('</strong>');
+                lineSections[0] = lineSections[0].replace('<strong>', '**').replace('.', ':**');
+                lineSections[1] = '<div class=cheerioLoad>' + lineSections[1] + '</div>';
+                var lineSectionsCheerio = CHEERIO.load(lineSections[1]);
+                lineSections[1] = lineSectionsCheerio('.cheerioLoad').text();
+                lineSections[0] = '<div class=cheerioLoad>' + lineSections[0] + '</div>';
+                lineSectionsCheerio = CHEERIO.load(lineSections[0]);
+                lineSections[0] = lineSectionsCheerio('.cheerioLoad').text();
+                //check to make sure it isn't too long
+                if (lineSections[0].length > 1024) {
+                  lineSections[1] = lineSections[1].substring(1023) + '…';
+                }
+                //make sure nothing went wrong
+                if (lineSections.length == 2) {
+                  //Create Field
+                  embed.addField(lineSections[0], lineSections[1], false);
+                  fieldCount++; 
+                } 
+              }             
+            }
+      }
+      message.channel.stopTyping();
+      message.channel.send({embed});
+      return;
+    }); 
+  }
+}
 
 //Connect to Database
 sqldb.connect(function(err) {
