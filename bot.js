a04dda077547b1d481e0e2085e23fc3a7b5447e4
Silver0034 <airraid.mail@gmail.com>
2017-12-26 13:45:21 -0500

    Add files via upload

--- bot.js
+++ bot.js
@@ -235,6 +235,7 @@ commandDictionary['about'] = {
       .addField('Information Sources', 'Much of the information this bot posts is original, but many commands relating to D&D reference or directly pull information from sources owned by *Wizards of the Coast*\nLinks are provided to the origonal information if it comes from a web-based source.')
       .addField('NPMs', 'Much of the code used to power ' + BOT.user.username + '™ is authored by the creators above. This bot runs Discord.js via Node.js and *does* utilize open-sourced NPMs below' + '```Cheerio, Jimp, Mysql, Scrape-It ```')
       .addField('Artwork', 'All artwork specific to ' + BOT.user.username + '™ including but not limited to its logos and icons are origonal pieces. Artworks pertaining to information owned by *Wizards of the Coast* are often pulled from the same sources as the information they accompany.')
+      .addField('Legal', BOT.user.username + '™ is owned by and run by Jacob Lodes. The name' + BOT.user.username + '™ is protected by Florida Trademark law')
       .setFooter(embedFooter)
       .addBlankField(false)
       .setThumbnail(commandDictionary[key].icon);
