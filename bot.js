78d0a40b4361a27818a53ba351422aef0fd29ce7
Silver0034 <airraid.mail@gmail.com>
2017-12-26 20:08:36 -0500

    Add files via upload

--- bot.js
+++ bot.js
@@ -599,6 +599,7 @@ commandDictionary['nick'] = {
       //Pull toggle number from database
       sqldb.query("SELECT * FROM user WHERE userID = " + message.author.id, function (err, results, fields) {
 		  var nicknameToggleState = results[0].nicknameToggle;
+			debugLog('nicknameToggleState = ' = nicknameToggleState);
       var nickname = ''; 
       });
                
