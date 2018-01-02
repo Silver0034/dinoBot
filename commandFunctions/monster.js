acef61593bd28d05a69e927bff80b7c33306124f
Silver0034 <airraid.mail@gmail.com>
2018-01-02 14:34:14 -0500

    Add files via upload

--- commandFunctions/monster.js
+++ commandFunctions/monster.js
@@ -28,7 +28,7 @@ exports.specific = function(message, key, emoji, commandDictionary, debugLog, BO
 			listItem: ".ability-block__heading",
 		},
 		attribute: {
-			listItem: ".mon-stat-block__attribute",
+			selector: ".mon-stat-block__attribute",
 			how: "html",
 		},
 		attributeTitle: {
